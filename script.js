d1aec664b6e4d0e5c38d625d620aa2ea92254073
Hani <hani.beldjouhri@gmail.com>
2023-12-30 17:19:55 +0100

    i add an alert with css,Html,js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 let input = document.querySelector("input");
-let button = document.querySelector("button");
+let button_IFR = document.querySelector("button");
+let button_Alt = document.getElementById("button_Alt");
 let iframe = document.querySelector("iframe");
+let Alert = document.getElementById("Alert");
+let overlay = document.getElementById("overlay");
 let youtubeUrl = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/(watch\?v=)?([A-Za-z0-9_-]+)$/;
 
 
-button.addEventListener("click",()=>{
+button_IFR.addEventListener("click",()=>{
     if(youtubeUrl.test(input.value)){
         let updatedUrl = input.value.replace(/(https?:\/\/)?(www\.)?youtube\.com\/watch\?v=/, "https://www.youtube.com/embed/");
         iframe.src = updatedUrl
     }else{
-        let h = document.createElement("h2")
-        h.textContent = "WRONG"
-        h.style.color = "red"   
-        document.body.append(h)
+        Alert.style.display = "block"
+        overlay.style.display = "block"
     }
+
+})
+button_Alt.addEventListener("click",()=>{
+    Alert.style.display = "none"
+    overlay.style.display = "none"
 
 })
